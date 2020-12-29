e61d438b36638a95877527fd9372ac464164d43c
martijnxd <49381565+martijnxd@users.noreply.github.com>
2020-12-29 08:43:54 +0100

    add ps script and change boot to efi

---
+++ windows/2019/deploy-2019-template.ps1
@@ -0,0 +1,10 @@
+# Powershell deployment script for packer
+# 
+# Ask for credentials vcenter and local admin password (should be the same as in unattended.xml)
+# and pass them to packer
+###
+$credentials = get-credential
+$var_file = ".\variables.json"
+$build_file = "win2019.standard"
+$winadmin_password = Read-Host 'enter local admin password'
+packer build --var-file $var_file -var "vcenter_username=$($Credentials.username)"  -var "vcenter_password=$($Credentials.GetNetworkCredential().Password)"  -var "winadmin-password=$winadmin_password" $build_file
