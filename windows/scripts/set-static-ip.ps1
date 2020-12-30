ade4965577eee8938d18a25010e00419ba98a042
martijnxd <49381565+martijnxd@users.noreply.github.com>
2020-12-30 14:40:57 +0100

    removed if

--- windows/scripts/set-static-ip.ps1
+++ windows/scripts/set-static-ip.ps1
@@ -1,3 +1,3 @@
 param($IP, $GW, $dns1, $dns2)
 New-NetIPAddress -InterfaceAlias Ethernet0 -IPAddress $IP -AddressFamily IPv4 -PrefixLength 24 -DefaultGateway $GW
-if ($dns2) { get-netadapter -ifAlias Ethernet0 | Set-DnsClientServerAddress -ServerAddresses @($dns1, $dns2) } else { get-netadapter -ifAlias Ethernet0 | Set-DnsClientServerAddress -ServerAddresses @($dns1) }
+get-netadapter -ifAlias Ethernet0 | Set-DnsClientServerAddress -ServerAddresses @($dns1, $dns2)
