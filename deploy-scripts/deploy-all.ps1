fe648d16dc47c84b3748a15dcc21d5becf9b17e0
martijnxd <martijn.ploeg@gmail.com>
2021-01-09 16:27:52 +0000

    added centos 8

--- deploy-scripts/deploy-all.ps1
+++ deploy-scripts/deploy-all.ps1
@@ -3,4 +3,5 @@ $local_admin_pass=Read-Host "Enter local administrator password"
 $params = @{credential=$credential;local_admin_pass=$local_admin_pass}
 & "$psscriptroot\deploy-2019-core.ps1"  @params
 & "$psscriptroot\deploy-2019-standard.ps1" @params
-& "$psscriptroot\deploy-ubuntu-20.4-server.ps1" @params
+& "$psscriptroot\deploy-ubuntu-20.4.ps1" @params
+& "$psscriptroot\deploy-centos-8.ps1" @params
