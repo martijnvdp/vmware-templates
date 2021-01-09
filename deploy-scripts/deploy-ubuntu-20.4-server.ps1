bf36c91498cacbfdbe2d3d5620637b6e9634e782
martijnxd <martijn.ploeg@gmail.com>
2021-01-09 10:04:36 +0000

    cleanup

--- deploy-scripts/deploy-ubuntu-20.4-server.ps1
+++ deploy-scripts/deploy-ubuntu-20.4-server.ps1
@@ -1,15 +1,20 @@
+Param($template_varfile)
 # Powershell deployment script for packer
 # 
 # Ask for credentials vcenter and local admin password
 # and pass them to packer
-#####
-$template_path = $PSscriptroot + "\..\ubuntu\20.4"
-$template_var_path = $PSscriptroot + "\..\..\vars"
-#####
+
 . "$PSScriptRoot\function\publish-template.ps1"
-if (!(test-path $template_var_path"\ubuntu-20.04-server.variables.json")) { $template_var_path = $template_path }
+#####
+$template_file = $PSscriptroot + "\..\templates\ubuntu\20.4\server-efi.json"
+$template_varfile= $PSscriptroot+ "\..\templates\ubuntu\20.4\server-efi.variables.json"
+#$builder_var_file= $PSscriptroot + "\..\builders\vsphere-iso\vsphere-iso.variables.json"
+$builder_var_file= $PSscriptroot + "\..\..\vars\vsphere-iso.variables.json"
+#####
+
 publish-template `
-    -template_os "ubuntu" `
-    -Template_file $template_path"\ubuntu-20.04-server.json"  `
-    -Template_var_file $template_var_path"\ubuntu-20.04-server.variables.json" `
+    -Template_os "ubuntu" `
+    -Template_file $template_file  `
+    -Template_var_file $template_varfile `
+    -Builder_var_file $builder_var_file `
     -template_path_packer "c:\packer" 
