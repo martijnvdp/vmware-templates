4f6f1d42e7425ca3fe898af21a596bf8cd40be56
martijnxd <martijn.ploeg@gmail.com>
2021-01-10 17:47:35 +0100

    ssh

--- templates/Windows/scripts/config2.ps1
+++ templates/Windows/scripts/config2.ps1
@@ -55,6 +55,11 @@ Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" 
 
 #time zone
 Set-TimeZone -Name "W. Europe Standard Time"
+
+#ssh
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+
 # Install trusted CA certificates
 
 # Get certificates
