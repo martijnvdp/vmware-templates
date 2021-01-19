69bc8e8519ec470b9df526e4981692f7596ab5ad
martijnvdp <m.vanderploeg@nl.ccv.eu>
2021-01-19 15:43:22 +0100

    fixed builder parameter file reference https://github.com/martijnxd/vmware-templates/issues/20

--- deploy-scripts/deploy-centos-8.ps1
+++ deploy-scripts/deploy-centos-8.ps1
@@ -14,5 +14,5 @@ $deploy_params=@{
     template_path_packer="c:\packer" 
 }
 #####
-if (!(test-path $deploy_params.builder_var_file)){$PSscriptroot + "\..\builders\vsphere-iso\vsphere-iso.variables.json"} 
+if (!(test-path $deploy_params.builder_var_file)){$deploy_params.builder_var_file=$PSscriptroot + "\..\builders\vsphere-iso\vsphere-iso.variables.json"} 
 publish-template @deploy_params -Credential $credential 
