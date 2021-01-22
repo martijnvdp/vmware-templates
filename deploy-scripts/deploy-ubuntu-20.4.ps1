e43fcc1cad9b1844273530ff0fe9a9a271672a33
martijnxd <martijn.ploeg@gmail.com>
2021-01-22 23:11:23 +0100

    format

--- deploy-scripts/deploy-ubuntu-20.4.ps1
+++ deploy-scripts/deploy-ubuntu-20.4.ps1
@@ -1,4 +1,4 @@
-Param($template_varfile,[pscredential]$credential)
+Param($template_varfile, [pscredential]$credential)
 # Powershell deployment script for packer
 # 
 # Ask for credentials vcenter and local admin password
@@ -6,13 +6,13 @@ Param($template_varfile,[pscredential]$credential)
 
 . "$PSScriptRoot/function/publish-template.ps1"
 #####
-$deploy_params=@{
-    template_file = $PSscriptroot + "/../templates/Ubuntu/20.4/server.json"
-    template_var_file= $PSscriptroot+ "/../templates/Ubuntu/20.4/server.variables.json"
-    Template_os="linux"
-    builder_var_file= $PSscriptroot + "/../../vars/vsphere-iso.variables.json"
-    template_path_packer="c:\packer" 
+$deploy_params = @{
+    template_file        = $PSscriptroot + "/../templates/Ubuntu/20.4/server.json"
+    template_var_file    = $PSscriptroot + "/../templates/Ubuntu/20.4/server.variables.json"
+    Template_os          = "linux"
+    builder_var_file     = $PSscriptroot + "/../../vars/vsphere-iso.variables.json"
+    template_path_packer = "c:\packer" 
 }
 #####
-if (!(test-path $deploy_params.builder_var_file)){$deploy_params.builder_var_file=$PSscriptroot + "/../builders/vsphere-iso/vsphere-iso.variables.json"} 
+if (!(test-path $deploy_params.builder_var_file)) { $deploy_params.builder_var_file = $PSscriptroot + "/../builders/vsphere-iso/vsphere-iso.variables.json" } 
 publish-template @deploy_params -Credential $credential
